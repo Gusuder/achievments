366869c0f4b640b6f7692de33095fb9f25aaac77
Gusuder <58111097+Gusuder@users.noreply.github.com>
2025-12-14 12:45:48 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// GitHub Pages: https://gusuder.github.io/achievments/
 export default defineConfig({
   plugins: [react()],
-  base: '/achievments/',
-})
+  base: "/achievments/",
+});
